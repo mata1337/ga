53a731eb224c569d73fa5d89a07a1fa3d99037f6
mata <204466088+mata1337@users.noreply.github.com>
2026-06-06 22:53:52 +0200

    Refactor string indexing to use array syntax

--- forums/include/utf8/utils/bad.php
+++ forums/include/utf8/utils/bad.php
@@ -270,8 +270,7 @@ function utf8_bad_identify($str, &$i)
 
 	for($i=0; $i < $len; $i++)
 	{
-		$in = ord($str{$i});
-
+    $in = ord($str[$i]);
 		if ( $mState == 0)
 		{
 			// When mState is zero we expect either a US-ASCII character or a multi-octet sequence.
