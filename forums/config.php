617666dfa60b29da4314fe3b40f392665f8178a4
mata <204466088+mata1337@users.noreply.github.com>
2026-06-07 13:39:27 +0200

    Change database host, name, username, and password
    Updated database connection details and cookie settings.

--- forums/config.php
+++ forums/config.php
@@ -1,17 +1,14 @@
 <?php
-
 $db_type = 'mysqli';
-$db_host = 'localhost';
-$db_name = ''; // base
-$db_username = ''; //login bd
-$db_password = ''; //pass db
+$db_host = 'mysql.railway.internal';
+$db_name = 'railway';
+$db_username = 'root';
+$db_password = 'ECAtdulkjKyeZejOdJnIoySvxVQeCVje';
 $db_prefix = '';
 $p_connect = false;
-
-$cookie_name = 'gamesense_cookie_5123cb'; //cookie change  plz
+$cookie_name = 'gamesense_cookie_5123cb';
 $cookie_domain = '';
 $cookie_path = '/';
 $cookie_secure = 0;
-$cookie_seed = 'c85432f3d4f954ad'; // try too
-
+$cookie_seed = 'c85432f3d4f954ad';
 define('PUN', 1);
